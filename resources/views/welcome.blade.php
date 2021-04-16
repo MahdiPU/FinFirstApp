2015ab7f51216add635e4cbb14f636787840d9c3
HW <groovy1999@yahoo.com>
2021-04-16 05:53:03 +0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.layout')
+@section('content')
+    <div class="container d-grid align-content-center h-max justify-content-center">
+        <div class="text-center mt-5 ">
+            <h1 class="font-sans" style="font-size: 10rem;color: #ffffff">Welcome To My Task</h1>
+        </div>
+    </div>
+@endsection
